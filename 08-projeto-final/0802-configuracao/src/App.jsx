a7abd9127d32b26e4c59fcb70c9c798a9ef6c5d1
Fernanda Costa <fernandacosta@alu.ufc.br>
2024-07-28 20:35:04 -0300

    fix: remove old screen and components

--- 08-projeto-final/0802-configuracao/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App = () => {
-  return <div>Dogs</div>;
-};
-
-export default App;
--- 08-projeto-final/0820-feed/src/Components/Feed/FeedModal.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const FeedModal = () => {
-  return <div></div>;
-};
-
-export default FeedModal;
